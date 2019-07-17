4aa4ebbac6c56bace4d6f012bcaec24b3a034384
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-07-17 13:29:15 +0300

    sync upstream ref:b6922d0c521f8a36dc79903e54de77a05a072088

--- examples/src/main/java/com/yandex/ydb/examples/simple/SimpleExample.java
+++ examples/src/main/java/com/yandex/ydb/examples/simple/SimpleExample.java
@@ -11,31 +11,27 @@ import com.yandex.ydb.core.rpc.RpcTransport;
  */
 public abstract class SimpleExample {
 
-    private static final String HOST;
-    private static final int PORT;
-    private static final String PATH;
-
-    static {
-        HOST = System.getProperty("HOST", "solomon-dev-myt-00.search.yandex.net");
-        PORT = Integer.parseInt(System.getProperty("PORT", "2135"));
-
-        String path = System.getProperty("PATH", "/Solomon/");
-        PATH = path.endsWith("/") ? path : path + "/";
-    }
-
     protected void doMain() {
+        String host = System.getProperty("HOST", "some.host.name.com");
+        int port = Integer.parseInt(System.getProperty("PORT", "2135"));
+
+        String path = System.getProperty("PATH", "/Root/");
+        if (!path.endsWith("/")) {
+            path += "/";
+        }
+
         System.err.println("Run with -DHOST=<hostname> and -DPORT=<port> to override connection settings");
         System.err.println();
-        System.err.println("HOST=" + HOST);
-        System.err.println("PORT=" + PORT);
-        System.err.println("PATH=" + PATH);
+        System.err.println("HOST=" + host);
+        System.err.println("PORT=" + port);
+        System.err.println("PATH=" + path);
         System.err.println();
 
-        try (RpcTransport transport = GrpcTransportBuilder.singleHost(HOST, PORT)
+        try (RpcTransport transport = GrpcTransportBuilder.singleHost(host, port)
             .withExecutorService(Executors.newFixedThreadPool(3))
             .build())
         {
-            run(transport, PATH);
+            run(transport, path);
         } catch (Throwable t) {
             t.printStackTrace();
         }
