3af8daa1a37edfb8d9420c3e163fff4f4c6c7731
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-06-10 19:17:10 +0300

    sync upstream ref:5a0b1e9ccdc2a722e62577715e27404f15326682

---
+++ auth-iam/src/main/java/com/yandex/ydb/auth/iam/CloudAuthProvider.java
@@ -0,0 +1,40 @@
+package com.yandex.ydb.auth.iam;
+
+import com.yandex.ydb.core.auth.AuthProvider;
+import yandex.cloud.sdk.auth.provider.CredentialProvider;
+
+/**
+ * This provider is using CredentialProvider of Yandex Cloud SDK for Java.
+ * Usage example:
+ *
+ * <pre>{@code
+ * CredentialProvider provider = ApiKeyCredentialProvider.builder()
+ *      .fromFile(/path/to/json/file"")
+ *      .enableCache()
+ *      .build();
+ *
+ * GrpcTransport transport = GrpcTransport.forEndpoint("some-ydb-endpoint", "my_db")
+ *      .withAuthProvider(CloudAuthProvider.newAuthProvider(provider))
+ *      .build();
+ *
+ * } </pre>
+ *
+ * @author Vasilii Briginets
+ */
+public class CloudAuthProvider implements AuthProvider {
+    CredentialProvider credentialProvider;
+
+    private CloudAuthProvider(CredentialProvider credentialProvider) {
+        this.credentialProvider = credentialProvider;
+    }
+
+    public static CloudAuthProvider newAuthProvider(CredentialProvider provider) {
+        return new CloudAuthProvider(provider);
+    }
+
+    @Override
+    public String getToken() {
+        return credentialProvider.get()
+                .getToken();
+    }
+}
---
+++ auth-iam/src/test/java/com/yandex/ydb/auth/iam/CloudAuthProviderTest.java
@@ -0,0 +1,43 @@
+package com.yandex.ydb.auth.iam;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import yandex.cloud.sdk.auth.provider.ComputeEngineCredentialProvider;
+import yandex.cloud.sdk.auth.provider.CredentialProvider;
+
+
+/**
+ * @author Vasilii Briginets
+ */
+public class CloudAuthProviderTest {
+
+    @Rule
+    public MockServerRule mockServerRule = new MockServerRule(this);
+    private CredentialProvider credentialProvider;
+
+    @Before
+    public void setUp() {
+        mockServerRule.getClient()
+                .when(HttpRequest.request()
+                        .withMethod("GET")
+                        .withPath("/computeMetadata/v1/instance/service-accounts/default/token")
+                        .withHeader("Metadata-Flavor", "Google"))
+                .respond(HttpResponse.response("{\"access_token\":\"my-awesome-token\",\"expires_in\":10}"));
+
+
+        credentialProvider = ComputeEngineCredentialProvider.builder()
+                .metadataServerUrl("http://localhost:" + mockServerRule.getPort())
+                .build();
+    }
+
+    @Test
+    public void getToken() {
+        CloudAuthProvider authProvider = CloudAuthProvider.newAuthProvider(credentialProvider);
+        Assert.assertEquals("my-awesome-token", authProvider.getToken());
+    }
+}
