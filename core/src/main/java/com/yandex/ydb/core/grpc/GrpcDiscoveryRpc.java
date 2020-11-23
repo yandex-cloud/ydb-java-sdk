1bf311be4d7fb771281b95bd852a472f0ba18021
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-11-23 13:07:24 +0300

    sync upstream ref:f9a7cabd83a8948e8b76d6b66ec2eabbd3d33c83

---
+++ core/src/main/java/com/yandex/ydb/core/grpc/GrpcDiscoveryRpc.java
@@ -0,0 +1,47 @@
+package com.yandex.ydb.core.grpc;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+
+import com.yandex.ydb.core.Result;
+import com.yandex.ydb.core.rpc.OperationTray;
+import com.yandex.ydb.discovery.DiscoveryProtos.ListEndpointsRequest;
+import com.yandex.ydb.discovery.DiscoveryProtos.ListEndpointsResult;
+import com.yandex.ydb.discovery.v1.DiscoveryServiceGrpc;
+
+/**
+ * @author Vladimir Gordiychuk
+ */
+public class GrpcDiscoveryRpc implements AutoCloseable {
+    private final GrpcTransport transport;
+    private final OperationTray operationTray;
+
+    public GrpcDiscoveryRpc(GrpcTransport transport) {
+        this.transport = transport;
+        this.operationTray = transport.getOperationTray();
+    }
+
+    public CompletableFuture<Result<ListEndpointsResult>> listEndpoints(String database, long deadlineAfter) {
+        ListEndpointsRequest request = ListEndpointsRequest.newBuilder()
+                .setDatabase(database)
+                .build();
+
+        return transport.unaryCall(DiscoveryServiceGrpc.getListEndpointsMethod(), request, deadlineAfter)
+                .thenCompose(result -> {
+                    if (!result.isSuccess()) {
+                        return CompletableFuture.completedFuture(result.cast());
+                    }
+
+                    return operationTray.waitResult(
+                            result.expect("listEndpoints()").getOperation(),
+                            ListEndpointsResult.class,
+                            Function.identity(),
+                            deadlineAfter);
+                });
+    }
+
+    @Override
+    public void close() {
+        transport.close();
+    }
+}
