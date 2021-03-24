30c58f10e65e6785a2e35648d1ffd25d2159e4b1
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-03-24 17:06:23 +0300

    sync upstream ref:7a90975e2e0be902f03a2b5534df0abeb3d3e360

---
+++ table/src/main/java/com/yandex/ydb/table/description/StoragePool.java
@@ -0,0 +1,13 @@
+package com.yandex.ydb.table.description;
+
+public final class StoragePool {
+    private final String media;
+
+    public StoragePool(String media) {
+        this.media = media;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+}
