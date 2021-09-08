9da516ce1315e3fe977322ec003b9c98603b0237
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-09-08 19:29:36 +0300

    sync upstream ref:4682e758092add71e3c0b36edd99c2c631f09158

---
+++ auth-iam/src/main/java/com/yandex/ydb/auth/iam/CloudAuthHelper.java
@@ -0,0 +1,49 @@
+package com.yandex.ydb.auth.iam;
+
+import java.nio.file.Paths;
+
+import com.yandex.ydb.core.auth.AuthProvider;
+import com.yandex.ydb.core.auth.NopAuthProvider;
+import yandex.cloud.sdk.auth.provider.ApiKeyCredentialProvider;
+import yandex.cloud.sdk.auth.provider.ComputeEngineCredentialProvider;
+import yandex.cloud.sdk.auth.provider.IamTokenCredentialProvider;
+
+public class CloudAuthHelper {
+    public static AuthProvider getAuthProviderFromEnviron() {
+        String saKeyFile = System.getenv("YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS");
+        if (saKeyFile != null) {
+            return CloudAuthProvider.newAuthProvider(
+                ApiKeyCredentialProvider.builder()
+                     .fromFile(Paths.get(saKeyFile))
+                    .build()
+            );
+        }
+
+        String anonCredentials = System.getenv("YDB_ANONYMOUS_CREDENTIALS");
+        if (anonCredentials != null && anonCredentials == "1") {
+            return NopAuthProvider.INSTANCE;
+        }
+
+        String metadataCredentials = System.getenv("YDB_METADATA_CREDENTIALS");
+        if (metadataCredentials != null && metadataCredentials == "1") {
+            return CloudAuthProvider.newAuthProvider(
+                    ComputeEngineCredentialProvider.builder()
+                    .build()
+            );
+        }
+
+        String accessToken = System.getenv("YDB_ACCESS_TOKEN_CREDENTIALS");
+        if (accessToken != null) {
+            return CloudAuthProvider.newAuthProvider(
+                    IamTokenCredentialProvider.builder()
+                    .token(accessToken)
+                    .build()
+            );
+        }
+
+        return CloudAuthProvider.newAuthProvider(
+                ComputeEngineCredentialProvider.builder()
+                        .build()
+        );
+    }
+}
