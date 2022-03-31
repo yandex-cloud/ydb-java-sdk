6e71fabbb8adb6b1cb35cda121457ac8f36709c4
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2022-03-31 16:18:33 +0300

    sync upstream ref:f2c48eae985f4f1037a1e3db5718c3c6db21848d

--- examples/simple_project/src/main/java/com/yandex/ydb/example/Main.java
+++ examples/simple_project/src/main/java/com/yandex/ydb/example/Main.java
@@ -1,13 +1,12 @@
 package com.yandex.ydb.example;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 import com.yandex.ydb.auth.iam.CloudAuthHelper;
+import com.yandex.ydb.core.UnexpectedResultException;
 import com.yandex.ydb.core.grpc.GrpcTransport;
 import com.yandex.ydb.table.SessionRetryContext;
 import com.yandex.ydb.table.TableClient;
-import com.yandex.ydb.table.description.TableColumn;
 import com.yandex.ydb.table.description.TableDescription;
 import com.yandex.ydb.table.query.Params;
 import com.yandex.ydb.table.result.ResultSetReader;
@@ -15,20 +14,25 @@ import com.yandex.ydb.table.rpc.grpc.GrpcTableRpc;
 import com.yandex.ydb.table.transaction.TxControl;
 import com.yandex.ydb.table.values.PrimitiveType;
 import com.yandex.ydb.table.values.PrimitiveValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public final class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final String TABLE_NAME = "simple_project";
+
     private Main() { }
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
-            System.err.println("Usage: java -jar simple-project.jar <endpoint>");
+            System.err.println("Usage: java -jar ydb-simple-project.jar <connection-string>");
             return;
         }
 
-        String endpoint = args[0];
+        String connectionString = args[0];
 
-        GrpcTransport transport = GrpcTransport.forConnectionString(endpoint)
+        GrpcTransport transport = GrpcTransport.forConnectionString(connectionString)
                 .withAuthProvider(CloudAuthHelper.getAuthProviderFromEnviron())
                 .build();
 
@@ -36,13 +40,17 @@ public final class Main {
         try (TableClient tableClient = TableClient.newClient(rpc).build()) {
             SessionRetryContext retryCtx = SessionRetryContext.create(tableClient).build();
 
-            execute(retryCtx, transport.getDatabase());
+            createTable(retryCtx, transport.getDatabase(), TABLE_NAME);
+            describeTable(retryCtx, transport.getDatabase(), TABLE_NAME);
+            upsertData(retryCtx, TABLE_NAME);
+            selectData(retryCtx, TABLE_NAME);
+            dropTable(retryCtx, transport.getDatabase(), TABLE_NAME);
+        } catch (UnexpectedResultException e) {
+            logger.error("Unexpected", e);
         }
     }
 
-    private static void execute(SessionRetryContext retryCtx, String database) {
-        String tablePath = database + "/" + "java_example";
-
+    private static void createTable(SessionRetryContext retryCtx, String database, String tableName) {
         TableDescription pets = TableDescription.newBuilder()
                 .addNullableColumn("species", PrimitiveType.utf8())
                 .addNullableColumn("name", PrimitiveType.utf8())
@@ -51,24 +59,29 @@ public final class Main {
                 .setPrimaryKeys("species", "name")
                 .build();
 
+        String tablePath = database + "/" + tableName;
         retryCtx.supplyStatus(session -> session.createTable(tablePath, pets))
                 .join().expect("ok");
+    }
 
+    private static void describeTable(SessionRetryContext retryCtx, String database, String tableName) {
+        String tablePath = database + "/" + tableName;
         TableDescription desc = retryCtx.supplyResult(session -> session.describeTable(tablePath))
                 .join().expect("ok");
-        System.out.println("Columns reported by describeTable: " + desc.getColumns().stream()
-                .map(TableColumn::getName).collect(Collectors.toList()));
+        logger.info("Columns reported by describeTable:");
+        desc.getColumns().forEach(column -> {
+            logger.info(" {} : {}", column.getName(), column.getType());
+        });
+    }
 
-        String upsertQuery = String.format(
-                "PRAGMA TablePathPrefix(\"%s\");\n" +
-                        "\n" +
-                        "DECLARE $species AS Utf8;\n" +
-                        "DECLARE $name AS Utf8;\n" +
-                        "DECLARE $color AS Utf8;\n" +
-                        "DECLARE $price AS Float;\n" +
-                        "UPSERT INTO java_example (species, name, color, price) VALUES\n" +
-                        "($species, $name, $color, $price);",
-                database);
+    private static void upsertData(SessionRetryContext retryCtx, String tableName) {
+        String upsertQuery
+                = "DECLARE $species AS Utf8;"
+                + "DECLARE $name AS Utf8;"
+                + "DECLARE $color AS Utf8;"
+                + "DECLARE $price AS Float;"
+                + "UPSERT INTO " + tableName + " (species, name, color, price)"
+                + "VALUES($species, $name, $color, $price);";
 
         Params params = Params.of(
                 "$species", PrimitiveValue.utf8("cat"),
@@ -76,32 +89,41 @@ public final class Main {
                 "$color", PrimitiveValue.utf8("black"),
                 "$price", PrimitiveValue.float32(10.0f)
         );
-        TxControl txRW = TxControl.serializableRw().setCommitTx(true);
-        retryCtx.supplyResult(session -> session.executeDataQuery(upsertQuery, txRW, params))
+        TxControl tx = TxControl.serializableRw().setCommitTx(true);
+        retryCtx.supplyResult(session -> session.executeDataQuery(upsertQuery, tx, params))
                 .join().expect("ok");
+    }
 
-        String selectQuery = String.format(
-                "PRAGMA TablePathPrefix(\"%s\");\n" +
-                        "\n" +
-                        "DECLARE $species AS Utf8;\n" +
-                        "DECLARE $name AS Utf8;\n" +
-                        "SELECT * FROM java_example\n" +
-                        "WHERE species = $species AND name = $name;",
-                database);
-        TxControl txRO = TxControl.onlineRo();
+    private static void selectData(SessionRetryContext retryCtx, String tableName) {
+        String selectQuery
+                = "DECLARE $species AS Utf8;"
+                + "DECLARE $name AS Utf8;"
+                + "SELECT * FROM " + tableName + " "
+                + "WHERE species = $species AND name = $name;";
+
+        Params params = Params.of(
+                "$species", PrimitiveValue.utf8("cat"),
+                "$name", PrimitiveValue.utf8("Tom")
+        );
+
+        TxControl tx = TxControl.onlineRo();
         ResultSetReader rsReader = retryCtx
-                .supplyResult(session -> session.executeDataQuery(selectQuery, txRO, params))
+                .supplyResult(session -> session.executeDataQuery(selectQuery, tx, params))
                 .join().expect("ok").getResultSet(0);
-        System.out.println();
-        System.out.println("Result of select query:");
-        while (rsReader.next()) {
-            System.out.printf("species: %s%n", rsReader.getColumn("species").getUtf8());
-            System.out.printf("name: %s%n", rsReader.getColumn("name").getUtf8());
-            System.out.printf("color: %s%n", rsReader.getColumn("color").getUtf8());
-            System.out.printf("price: %f%n", rsReader.getColumn("price").getFloat32());
-            System.out.println();
-        }
 
+        logger.info("Result of select query:");
+        while (rsReader.next()) {
+            logger.info("  species: {}, name: {}, color: {}, price: {}",
+                    rsReader.getColumn("species").getUtf8(),
+                    rsReader.getColumn("name").getUtf8(),
+                    rsReader.getColumn("color").getUtf8(),
+                    rsReader.getColumn("price").getFloat32()
+            );
+        }
+    }
+
+    private static void dropTable(SessionRetryContext retryCtx, String database, String tableName) {
+        String tablePath = database + "/" + tableName;
         retryCtx.supplyStatus(session -> session.dropTable(tablePath))
                 .join().expect("ok");
     }
