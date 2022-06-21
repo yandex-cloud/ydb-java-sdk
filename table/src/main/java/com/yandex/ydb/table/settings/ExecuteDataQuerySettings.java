9faa1c9c07b836a2127fcc0f145f48e475639525
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2022-06-21 15:26:00 +0300

    sync upstream ref:d13d5d1a88cdb4cd95c82f17b2a73947e609f7b9

--- table/src/main/java/com/yandex/ydb/table/settings/ExecuteDataQuerySettings.java
+++ table/src/main/java/com/yandex/ydb/table/settings/ExecuteDataQuerySettings.java
@@ -1,5 +1,11 @@
 package com.yandex.ydb.table.settings;
 
+import javax.annotation.Nonnull;
+
+import com.yandex.ydb.table.YdbTable;
+
+import static com.yandex.ydb.table.YdbTable.QueryStatsCollection.Mode.STATS_COLLECTION_NONE;
+
 /**
  * @author Sergey Polovko
  */
@@ -7,12 +13,24 @@ public class ExecuteDataQuerySettings extends RequestSettings<ExecuteDataQuerySe
 
     private boolean keepInQueryCache = false;
 
+    private YdbTable.QueryStatsCollection.Mode collectStats = STATS_COLLECTION_NONE;
+
     public boolean isKeepInQueryCache() {
         return keepInQueryCache;
     }
 
     public ExecuteDataQuerySettings keepInQueryCache() {
         keepInQueryCache = true;
+        return this;
+    }
+
+    @Nonnull
+    public YdbTable.QueryStatsCollection.Mode collectStats() {
+        return collectStats;
+    }
+
+    public ExecuteDataQuerySettings setCollectStats(@Nonnull YdbTable.QueryStatsCollection.Mode collectStats) {
+        this.collectStats = collectStats;
         return this;
     }
 }
