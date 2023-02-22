8d4f545133a203b7142aa5c708a747a0145d6694
egorlitvinenk <egorlitvinenk@yandex-team.com>
2023-02-22 16:11:57 +0300

    support virtual timestamps and retention period in client
    support virtual timestamps and retention period in client

--- table/src/main/java/com/yandex/ydb/table/settings/Changefeed.java
+++ table/src/main/java/com/yandex/ydb/table/settings/Changefeed.java
@@ -2,6 +2,7 @@ package com.yandex.ydb.table.settings;
 
 import java.util.Objects;
 
+import com.google.protobuf.Duration;
 import com.yandex.ydb.table.YdbTable;
 
 /**
@@ -12,11 +13,19 @@ public class Changefeed {
     private final String name;
     private final Mode mode;
     private final Format format;
+    private final boolean virtualTimestamps;
+    private final java.time.Duration retentionPeriod;
 
     public Changefeed(String name, Mode mode, Format format) {
+        this(name, mode, format, false, java.time.Duration.ofHours(24));
+    }
+
+    public Changefeed(String name, Mode mode, Format format, boolean virtualTimestamps, java.time.Duration retentionPeriod) {
         this.name = Objects.requireNonNull(name);
         this.mode = Objects.requireNonNull(mode);
         this.format = Objects.requireNonNull(format);
+        this.virtualTimestamps = virtualTimestamps;
+        this.retentionPeriod = retentionPeriod;
     }
 
     public String getName() {
@@ -31,11 +40,28 @@ public class Changefeed {
         return format;
     }
 
+    public boolean hasVirtualTimestamps() {
+        return virtualTimestamps;
+    }
+
+    public java.time.Duration getRetentionPeriod() {
+        return retentionPeriod;
+    }
+
     public YdbTable.Changefeed toProto() {
         return YdbTable.Changefeed.newBuilder()
                 .setName(name)
                 .setFormat(format.toProto())
                 .setMode(mode.toProto())
+                .setVirtualTimestamps(virtualTimestamps)
+                .setRetentionPeriod(toProto(retentionPeriod))
+                .build();
+    }
+
+    private static Duration toProto(java.time.Duration duration) {
+        return Duration.newBuilder()
+                .setSeconds(duration.getSeconds())
+                .setNanos(duration.getNano())
                 .build();
     }
 
